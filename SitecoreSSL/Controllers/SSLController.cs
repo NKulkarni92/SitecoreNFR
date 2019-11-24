fddb34193794f758214c909b712c957d4c7152b7
NKulkarni92 <44356298+NKulkarni92@users.noreply.github.com>
2019-11-24 18:15:36 +1300

    Initial commit

---
+++ SitecoreSSL/Controllers/SSLController.cs
@@ -0,0 +1,37 @@
+﻿using SitecoreSSL.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace SitecoreSSL.Controllers
+{
+    public class SSLController : Controller
+    {
+        public ActionResult SSL()
+        {
+            string url = "https://www.ssllabs.com/ssltest/analyze.html?d=" + Request.Url.Host;
+            string browserURL = "http://browsershots.org/" + Request.Url;
+            string seoURL = "https://app.neilpatel.com/en/seo_analyzer/site_audit?domain=" + Request.Url.Host + "&submit=Analyze+Website";
+            string pageSpeedURL="https://developers.google.com/speed/pagespeed/insights/?url="+ Request.Url.Host;
+            string digitalURL ="https://www.woorank.com/en/www/" + Request.Url.Host;
+            string loadURL = "https://app.loadimpact.com/k6/anonymous-test/custom?url=http%3A%2F%2F" + Request.Url.Host + "&utm_campaign=Anon%20Test&utm_source=marketing%20CTA&utm_medium=website";
+            var model = new SSLModel();
+            model.PageSpeedURL = pageSpeedURL;
+            model.LoadURL = loadURL;
+            model.URL = url;
+            model.BrowserURL = browserURL;
+            model.SEOURL = seoURL;
+            model.DigitalURL = digitalURL;
+            return View(model);
+        }
+    }
+}
---
+++ SitecoreSSL/Models/SSLModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreSSL.Models
+{
+    public class SSLModel
+    {
+        public string URL { get; set; }
+        public string BrowserURL { get; set; }
+        public string SEOURL { get; set; }
+        public string PageSpeedURL { get; set; }
+        public string DigitalURL { get; set; }
+        public string LoadURL { get; set; }
+    } 
+}
